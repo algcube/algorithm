e749b07c9d16339debdf5c2a5f678ac75c43f83b
Ho-Ya-Ho <rkdgh1056@gmail.com>
2023-09-21 21:18:03 +0900

    04 상하좌우

---
+++ src/main/java/ch04_implementation/상하좌우.java
@@ -0,0 +1,35 @@
+package ch04_implementation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class 상하좌우 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bf.readLine());
+        String[] input = bf.readLine().split(" ");
+
+        int x = 1;
+        int y = 1;
+
+        String[] moveType = {"R", "L", "D", "U"};
+        int[] dx = {0, 0, 1, -1};
+        int[] dy = {1, -1, 0, 0};
+
+        for (int i = 0; i < input.length; i++) {
+            int index = Arrays.asList(moveType).indexOf(input[i]);
+
+            int nx = x + dx[index];
+            int ny = y + dy[index];
+
+            //범위 내에 있을 경우만 update
+            if (nx >= 1 && nx <= N && ny >= 1 && ny <= N) {
+                x = nx;
+                y = ny;
+            }
+        }
+        System.out.println(x + " " + y);
+    }
+}
