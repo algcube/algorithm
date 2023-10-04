667c30b83eb520529225f915463e1f2c603e4f0e
csbiy <katd6@naver.com>
2023-10-04 13:11:36 +0900

    update

--- src/main/java/bj/silver/CS_14889.java
+++ src/main/java/bj/silver/CS_14889.java
@@ -5,54 +5,29 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-public class CS_14889 {
+class Solution {
 
-    static int minDiff = Integer.MAX_VALUE;
-    static int[][] graph;
-
-    public static void main(String[] args) throws IOException {
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-
-        graph = new int[N][N];
-        for (int i = 0; i < N; i++) {
-            graph[i] = Arrays.stream(br.readLine().split(" "))
-                             .mapToInt(Integer::parseInt).toArray();
-        }
-
-        int team = (N / 2);
-        boolean[] visited = new boolean[N];
-        dfs(0, team, visited);
-        System.out.println(minDiff);
+    public static void main(String[] args) {
+        long solution = new Solution().solution(4, 5, new int[]{1, 0, 3, 1, 2}, new int[]{0, 3, 0, 4, 0});
+        System.out.println("solution = " + solution);
     }
 
+    public long solution(int cap, int n, int[] deliveries, int[] pickups) {
 
-    public static void dfs(int person, int team, boolean[] visited) {
-        if (team == 0) {
-            int START = 0;
-            int LINK = 0;
-            for (int i = 0; i < visited.length; i++) {
-                for (int j = i + 1; j < visited.length; j++) {
-                    if (visited[i] != visited[j]) {
-                        continue;
-                    }
-                    if (visited[i]) {
-                        START += graph[i][j] + graph[j][i];
-                    } else {
-                        LINK += graph[i][j] + graph[j][i];
-                    }
+        long dist = 0;
+        long tmp = cap;
+        for(int i = deliveries.length -1 ; i >=0  ; i--){
+            while (deliveries[i] != 0){
+                deliveries[i] =(int) Math.max(0,  deliveries[i] - tmp);
+                tmp -=deliveries[i];
+                if (tmp <= 0){
+                    tmp = cap;
+                    dist+= ((i+1)*2);
                 }
             }
-            minDiff = Math.min(minDiff, Math.abs(START - LINK));
-            return;
         }
-        if (person >= graph.length) {
-            return;
-        }
-        visited[person] = true;
-        dfs(person + 1, team - 1, visited);
-        visited[person] = false;
-        dfs(person + 1, team, visited);
+
+        return dist;
     }
+
 }
---
+++ src/main/java/programmers/폰켓몬/Solution.java
@@ -0,0 +1,9 @@
+package programmers.폰켓몬;
+
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] nums) {
+        return Math.min((int) Arrays.stream(nums).distinct().count(), nums.length);
+    }
+}
