bf6dce34c7974ecc6b177e2d68ebd61b1ffbcc58
Ho-Ya-Ho <rkdgh1056@gmail.com>
2023-10-09 15:52:50 +0900

    programmers.HO.greedy.단속카메라

--- src/main/java/programmers/HO/greedy/단속카메라/Solution.java
+++ src/main/java/programmers/HO/greedy/단속카메라/Solution.java
@@ -8,13 +8,13 @@ class Solution {
         Arrays.sort(routes, (a, b) -> Integer.compare(a[1], b[1]));
         int arrive = -9999999;
 
-        for (int i = 0; i < routes.length; i++) {
+        for (int[] route : routes) {
             // 전에 단속카메라에 등록한 arrive가 routes내에 포함 한다면 continue
-            if (arrive >= routes[i][0] && arrive <= routes[i][1]) {
+            if (arrive >= route[0] && arrive <= route[1]) {
                 continue;
             }
             answer++;
-            arrive =routes[i][1];
+            arrive = route[1];
         }
 
         return answer;
