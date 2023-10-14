fc05febe26e722b1fd9469c77c7cb6507111a4d2
Ho-Ya-Ho <rkdgh1056@gmail.com>
2023-10-14 13:36:29 +0900

    programmers.HO.sort.K번쨰수

---
+++ src/main/java/programmers/HO/sort/K번쨰수/Solution.java
@@ -0,0 +1,21 @@
+package programmers.HO.sort.K번쨰수;
+
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        List<Integer> list = new ArrayList<>();
+
+        for (int i =0; i< commands.length; i++) {
+            for (int j = commands[i][0]; j <= commands[i][1]; j++) {
+                list.add(array[j-1]);
+            }
+            Collections.sort(list);
+            answer[i] = list.get(commands[i][2] - 1);
+            list.clear();
+        }
+
+        return answer;
+    }
+}
